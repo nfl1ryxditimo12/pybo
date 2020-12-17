67474680ca5f09d5d2e0c3f1848416e625f9ea76
seongsu <nfl1ryxditimo12@gmail.com>
2020-12-18 02:26:05 +0900

    끝

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = '2_ldet@rl+0bngw(g-q4ix2@(0z1-3u$bi6uj=btm#qtv=v@pj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.35.254.94']
+ALLOWED_HOSTS = ['3.35.254.94', 'seongsu.me']
 
 
 # Application definition
